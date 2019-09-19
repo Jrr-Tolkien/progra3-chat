29e631b226d5210c73319b56ebe6c6962eb3b334
Sebas V <jsvale.guerrero@gmail.com>
2019-09-18 18:44:05 -0600

    less trash

--- Code/app.js
+++ Code/app.js
@@ -1,12 +1,29 @@
 ﻿var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 var port = process.env.PORT || 1337;
 app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+});
+
+//io.on("connection", function (socket) {
+//    socket.on("send message", function (sent_msg, callback) {
+//       sent_msg = "[ " + getCurrentDate() + " ]: " + sent_msg;
+//
+//        io.sockets.emit("update messages", sent_msg);
+//        callback();
+//    });
+//});
 
 http.listen(port, function () {
     console.log('listening on *:' + port);
